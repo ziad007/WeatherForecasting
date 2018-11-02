49dc83c41f79859ec4bfa2d77977158f6c5dd39a
Ziad Bou Ismail <zbouismail@gmail.com>
2018-11-02 20:40:17 +0200

    Initial Commit

---
+++ WeatherForecastingTests/WeatherForecastingTests.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherForecastingTests.swift
+//  WeatherForecastingTests
+//
+//  Created by ziad Bou Ismail on 11/2/18.
+//  Copyright © 2018 Ziad. All rights reserved.
+//
+
+import XCTest
+@testable import WeatherForecasting
+
+class WeatherForecastingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
